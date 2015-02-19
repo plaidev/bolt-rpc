899ffe4a42e145d5978a76630bb03ee43534d6f6
Naoto Kato <naoto.kato@plaid.co.jp>
2015-02-19 05:41:53 +0000

    update client.js

--- js/client.js
+++ js/client.js
@@ -1,5 +1,5 @@
 (function() {
-  var Client, Emitter, TrackClient, TrackCursor,
+  var Client, Cursor, Emitter, TrackClient, TrackCursor, buildChain,
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
@@ -11,10 +11,10 @@
 
   Client = require('minimum-rpc').Client;
 
-  TrackCursor = (function(_super) {
-    __extends(TrackCursor, _super);
+  Cursor = (function(_super) {
+    __extends(Cursor, _super);
 
-    function TrackCursor(method, data, cb, client) {
+    function Cursor(method, data, cb, client) {
       this.method = method;
       this.data = data;
       this.cb = cb;
@@ -24,17 +24,17 @@
       this.mdls = [];
     }
 
-    TrackCursor.prototype.error = function(cb) {
+    Cursor.prototype.error = function(cb) {
       this.on('error', cb);
       return this;
     };
 
-    TrackCursor.prototype.end = function(cb) {
+    Cursor.prototype.end = function(cb) {
       this.on('end', cb);
       return this;
     };
 
-    TrackCursor.prototype.update = function(_data) {
+    Cursor.prototype.update = function(_data) {
       if (_data !== void 0) {
         this.data = _data;
       }
@@ -61,14 +61,93 @@
       return this;
     };
 
-    TrackCursor.prototype.map = function(mdl) {
+    Cursor.prototype.map = function(mdl) {
       this.mdls.push(mdl);
       return this;
     };
 
-    return TrackCursor;
+    return Cursor;
 
   })(Emitter);
+
+  buildChain = function(funcs, cb) {
+    var cur, err, next, val, _bind, _i, _len, _ref;
+    err = null;
+    val = void 0;
+    _bind = function(cur, next) {
+      return function(_err, _val) {
+        if (_err) {
+          err = _err;
+        }
+        if (_val) {
+          val = _val;
+        }
+        return cur(err, val, next);
+      };
+    };
+    next = function(err, val, next) {
+      return cb(err, val);
+    };
+    _ref = funcs.reverse();
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      cur = _ref[_i];
+      next = _bind(cur, next);
+    }
+    return next;
+  };
+
+  TrackCursor = (function(_super) {
+    __extends(TrackCursor, _super);
+
+    function TrackCursor(method, data, cb, client) {
+      var _cb;
+      this.pres = [];
+      this.posts = [];
+      this.tracking = false;
+      _cb = (function(_this) {
+        return function(err, val) {
+          var next;
+          _this.tracking = false;
+          next = buildChain(_this.posts, cb);
+          return next(err, val);
+        };
+      })(this);
+      TrackCursor.__super__.constructor.call(this, method, data, _cb, client);
+    }
+
+    TrackCursor.prototype.pre = function(func) {
+      this.pres.push(func);
+      return this;
+    };
+
+    TrackCursor.prototype.post = function(func) {
+      this.posts.push(func);
+      return this;
+    };
+
+    TrackCursor.prototype.update = function(_data, trackContext) {
+      var next;
+      if (_data !== void 0) {
+        this.data = _data;
+      }
+      if (this.tracking) {
+        return;
+      }
+      this.tracking = true;
+      next = buildChain(this.pres, (function(_this) {
+        return function(err, trackContext) {
+          if (err) {
+            return;
+          }
+          return TrackCursor.__super__.update.call(_this, _data);
+        };
+      })(this));
+      return next(null, trackContext);
+    };
+
+    return TrackCursor;
+
+  })(Cursor);
 
   TrackClient = (function(_super) {
     __extends(TrackClient, _super);
@@ -77,13 +156,13 @@
       TrackClient.__super__.constructor.call(this, io_or_socket, options);
       this._cursors = [];
       this._socket.on(this.sub_name_space + '_track', (function(_this) {
-        return function(data) {
+        return function(trackContext) {
           var cursor, _i, _len, _ref, _results;
           _ref = _this._cursors;
           _results = [];
           for (_i = 0, _len = _ref.length; _i < _len; _i++) {
             cursor = _ref[_i];
-            _results.push(cursor.update());
+            _results.push(cursor.update(void 0, trackContext));
           }
           return _results;
         };
